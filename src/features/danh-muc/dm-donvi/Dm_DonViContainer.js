990690259f194cd7df207e012b62d4f0f2a8a4ca
Phạm Văn Tùng <pvtung.197@gmail.com>
2021-04-15 00:54:54 +0700

    f

--- src/features/danh-muc/dm-donvi/Dm_DonViContainer.js
+++ src/features/danh-muc/dm-donvi/Dm_DonViContainer.js
@@ -1,28 +1,36 @@
-import { DataGrid } from 'devextreme-react';
-import { Column } from 'devextreme-react/data-grid';
-import Form from 'devextreme-react/form';
+import { DataGrid, DateBox, DropDownBox, TextArea, TextBox, TreeList } from 'devextreme-react';
+import { Column, RequiredRule } from 'devextreme-react/data-grid';
+import Form, { Label, SimpleItem } from 'devextreme-react/form';
 import NumberBox from 'devextreme-react/number-box';
 import { Lookup, DropDownOptions } from 'devextreme-react/lookup';
 
 
-import { Dialog } from 'evergreen-ui';
+import { Dialog, Heading } from 'evergreen-ui';
 import React, { PureComponent } from 'react'
 import HeaderPage from '../../../components/header-page/HeaderPage';
 import SearchBar from '../../../components/search-bar/SearchBar';
 import Dm_DonViService from './Dm_DonViService';
 import MainGrid from './local-components/MainGrid';
+import { ColumnLookup } from 'devextreme-react/tree-list';
 
 
 
 export class Dm_DonViContainer extends PureComponent {
     constructor(props){
         super(props);
+        this.mainGridRef = null;
         this.state = {
             dataSource: [],
             diaBanData: [],
             donViData: [],
             linhVuc: [],
-            isLoading: fasle
+            isLoading: false,
+            selectedTemplateKey: {
+                diaBan: []
+            },
+            selectedValue: null,
+            mode: 'view',
+            isShowModel: true,
         }
     }
     componentDidMount(){
@@ -31,54 +39,131 @@ export class Dm_DonViContainer extends PureComponent {
     }
 
     handleSearch = (text) => {
-        console.log('text search : ', text);
+        const { gridRef } = this.mainGridRef;
+        gridRef.instance.searchByText(text);
     }
     handleAtions = (action) => {
         console.log('action : ', action);
+        if(action === 'Sua') {
+            this.setState((oldState) => {
+                return { isShowModel: true }
+            })
+        }
     }
     loadDonVi = () => {
         Dm_DonViService.getAll().then(res => {
-            
             this.setState({
                 dataSource: res.data
             })
         });
     }
+
+
+
     loadLookupData = () => {
         this.setState({isLoading: false});
          Dm_DonViService.getAllDiaBan().then(res => {
-             console.log('res : ', res);
              this.setState({
                 diaBanData: res.data
              })
          })
          Dm_DonViService.getAllLinhLuc().then(res => {
-            console.log('res linh vuc: ', res);
             this.setState({
                 linhVuc: res.data
              })
          })
     }
+    selectedTemplateKeyChanged = (e, tree) => {
+        const { selectedTemplateKey } = this.state;
+        if(tree === 'dia-ban'){
+            this.setState({
+                selectedTemplateKey: {
+                    ...selectedTemplateKey,
+                    diaBan: e
+                }
+            })
+        }
+    }
+
+    selectedRowChanged = (e) => {
+        let data = e.selectedRowsData[0];
+        this.setState({ selectedValue: data});
+    }
+
+    componentDidUpdate(){
+        console.log('update')
+    }
+
+    diaBanDropDownTemplate = () => {
+        return (
+            <TreeList
+                dataSource={this.state.diaBanData}
+                keyExpr="Id"
+                parentIdExpr="DBCha"
+                searchPanel={{visible:true}}
+                height={300}
+                showBorders={true}
+                showColumnHeaders={true}
+                showRowLines={true}
+                selection={{mode: 'single'}}
+                selectedRowKeys={this.state.selectedTemplateKey.diaBan}
+                onSelectedRowKeysChange={(e) => this.selectedTemplateKeyChanged(e, 'dia-ban')}
+            >
+                <Column dataField="MaDB" caption="Mã địa bàn"/>
+                <Column dataField="TenDB" caption="Tên địa bàn"/>
+            </TreeList>
+        )
+    }
+
     render() {
         return (
             <div className="page-contaienr">
                <HeaderPage  onAction={this.handleAtions} />
                <SearchBar title={'Danh mục đơn vị kiểm toán'} onSearch={this.handleSearch}  />
                <div>
-                   <MainGrid dataSource={this.state.dataSource} diaBanData={this.state.diaBanData} linhVucData={this.state.linhVuc}  />
+                   <MainGrid ref={ref => this.mainGridRef = ref} dataSource={this.state.dataSource} diaBanData={this.state.diaBanData} linhVucData={this.state.linhVuc} selectedRowChanged={this.selectedRowChanged}  />
                    <Dialog 
                         title="Thêm đơn vị"
                         confirmLabel="Lưu"
                         cancelLabel="Hủy"
-                        isShown={true}
-                        minHeightContent='300px'
+                        isShown={this.state.isShowModel}
+                        onCloseComplete={() => this.setState({isShowModel: false})}
                         width={'60%'}
-                        header={() => <h1 style={{
-                            fontSize:26
-                        }}>Thêm mới dữ liệu</h1>}
+                        header={() => <Heading >Thêm mới đơn vị</Heading>}
                     >
                        <div>
-                           
+                           <Form colCount={12}
+                                formData={{  ...this.state.selectedValue }}
+                            >
+                               <SimpleItem colSpan={6}  editorType="dxTextBox" dataField="Ma" label={{text: 'Mã đơn vị'}}>
+                                   <RequiredRule message="Mã đơn vị là trường bắt buộc"/>
+                               </SimpleItem>
+                               <SimpleItem colSpan={6} editorType="dxDropDownBox" dataField="IdCha"  label={{text: 'Đơn vị cha'}}>
+                                   
+                               </SimpleItem>
+                               <SimpleItem colSpan={6} editorType="dxTextBox" dataField="Ten" label={{text: 'Tên đơn vị'}}>
+                                    <RequiredRule message="Đơn vị là trường bắt buộc"/>
+                               </SimpleItem>
+                               <SimpleItem colSpan={6} editorType="dxDropDownBox" dataField="DiaBan"  label={{text: 'Địa bàn'}} >
+                                    <DropDownBox dataSource={this.state.diaBanData} value={this.state.selectedValue?.DiaBan}  valueExpr="Id" displayExpr="TenDB"  contentRender={this.diaBanDropDownTemplate}/>
+                                    <RequiredRule message="Địa bàn là trường bắt buộc"/>
+                               </SimpleItem>
+                               <SimpleItem colSpan={6} editorType="dxDateBox" dataField="NgayBatDau" editorOptions={{ displayFormat: 'dd/MM/yyyy'}} label={{text: 'Ngày bắt đầu'}}>
+                                   <RequiredRule message="Ngày bắt đầu là trường bắt buộc"/>
+                               </SimpleItem>
+                               
+                               <SimpleItem colSpan={6} editorType="dxDropDownBox"  label={{text: 'Lĩnh vực'}}>
+                                    <RequiredRule message="Lĩnh vực là trường bắt buộc"/>
+                               </SimpleItem>
+                               <SimpleItem colSpan={6} editorType="dxDateBox" dataField="NgayKetThuc" editorOptions={{ displayFormat: 'dd/MM/yyyy'}} label={{text: 'Ngày bắt đầu'}}>
+                                   <Label text="Ngày kết thúc"   />
+                                   <RequiredRule message="Ngày bắt đầu là trường bắt buộc"/>
+                               </SimpleItem>
+                               <SimpleItem colSpan={6} dataField="Sdt" editorType="dxTextBox" label={{text: 'Số điện thoại'}}>
+                               </SimpleItem>
+                               <SimpleItem colSpan={12} editorType="dxTextArea" label={{text: 'Ghi chú'}}>
+                               </SimpleItem>
+                           </Form>
                        </div>
                    </Dialog>
                </div>
--- src/features/danh-muc/dm-donvi/local-components/MainGrid.js
+++ src/features/danh-muc/dm-donvi/local-components/MainGrid.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react'
-import { DataGrid, TreeList } from 'devextreme-react';
+import React, { PureComponent } from 'react'
+import { DataGrid } from 'devextreme-react';
 import { Column, Lookup } from 'devextreme-react/data-grid';
+import TreeList,{
+    Pager,
+    Paging,
+  } from 'devextreme-react/tree-list';
 
 
-export class MainGrid extends Component {
+export class MainGrid extends PureComponent {
     constructor(props) {
         super(props);
+        this.gridRef = null;
     }
     render() {
         console.log(this.props.diaBanData, 'dia ban');
         return (
             <>
                 <TreeList
+                    ref={ref => this.gridRef = ref}
                     dataSource={this.props.dataSource || []}
                     showRowLines={true}
                     showBorders={true}
@@ -20,19 +26,25 @@ export class MainGrid extends Component {
                     selection={{ mode: 'single' }}
                     keyExpr="Id"
                     parentIdExpr="IdCha"
+                    allowColumnResizing={true}
+                    allowColumnReordering={true}
+                    height={500}
+                    onSelectionChanged={this.props.selectedRowChanged}
                 >
-                    <Column dataField="Ma" caption="Mã đơn vị" />
-                    <Column dataField="Ten" caption="Tên đơn vị" />
+                    <Column dataField="Ma" caption="Mã đơn vị" width={140} />
+                    <Column dataField="Ten" caption="Tên đơn vị" width={240} />
                     <Column dataField="DiaBan" caption="Địa bàn" >
                         <Lookup dataSource={this.props.diaBanData} valueExpr="Id" displayExpr="TenDB" />
                     </Column>
                     <Column dataField="LinhVuc" caption="Lĩnh vực" >
                         <Lookup dataSource={this.props.linhVucData} valueExpr="Id" displayExpr="Ten" />
                     </Column>
-                    <Column dataField="NgayBatDau" caption="Ngày bắt đầu" />
-                    <Column dataField="NgayKetThuc" caption="Ngày kết thúc" />
-                    <Column dataField="Sdt" caption="Số điện thoại" />
+                    <Column dataField="NgayBatDau" width={160} caption="Ngày bắt đầu" dataType='date' alignment={'center'} format={'dd/MM/yyyy'} />
+                    <Column dataField="NgayKetThuc" width={160} caption="Ngày kết thúc" dataType='date' alignment={'center'} format={'dd/MM/yyyy'} />
+                    <Column dataField="Sdt" caption="Số điện thoại" width={140} />
                     <Column dataField="GhiChu" caption="Ghi chú" />
+                    <Pager allowedPageSizes={[4, 50, 100]} showPageSizeSelector={true} />
+                    <Paging defaultPageSize={4} />
                 </TreeList>
             </>
         )
